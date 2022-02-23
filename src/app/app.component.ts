40a4501187e367cc35cd7acf67ede3d2789002db
Marwane MEHL <marwane.mehl@niji.fr>
2022-02-23 14:44:08 +0100

    Add material module and text areas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testApp';
+  anagramRes: string | undefined;
+  pangramRes: string | undefined;
+  counterRes: string | undefined;
+  anagramCode: string | undefined;
+  pangramCode: string | undefined;
+  counterCode: string | undefined;
+  counterSpinner = false;
+  pangramSpinner = false
+  anagramSpinner = false
+  firstTime = 0
+  constructor(private http: HttpClient) {}
+  ngOnInit() {}
+
+  testCounter() {
+    this.counterSpinner = true
+    this.counterRes = ''
+    this.http
+      .post(
+        'http://localhost:5000/counter',
+        { code: this.counterCode },
+        { responseType: 'text' }
+      )
+      .subscribe((data) => {
+        this.counterRes = data;
+        this.counterSpinner = false
+      });
+  }
+  async testAnagram() {
+    this.anagramSpinner = true
+    this.anagramRes = ''
+    this.http
+      .post(
+        'http://localhost:5000/anagram',
+        { code: this.anagramCode },
+        { responseType: 'text' }
+      )
+      .subscribe((data) => {
+        console.log(data);
+        this.anagramSpinner = false
+        this.anagramRes = data;
+      });
+  }
+  async testPangram() {
+    this.pangramSpinner = true
+    this.pangramRes = ''
+    this.http
+      .post(
+        'http://localhost:5000/pangram',
+        { code: this.pangramCode },
+        { responseType: 'text' }
+      )
+      .subscribe((data) => {
+        console.log(data);
+        this.pangramSpinner = false
+        this.pangramRes = data;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
